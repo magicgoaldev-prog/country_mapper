9e9250398300511729393b5d36af1e960285d617
magicgoal <magicgoal.dev@gmail.com>
2023-02-23 11:23:51 -0300

    starting project

---
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.plcoding.graphqlcountriesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.plcoding.graphqlcountriesapp.ui.theme.GraphQlCountriesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GraphQlCountriesAppTheme {
+
+            }
+        }
+    }
+}
