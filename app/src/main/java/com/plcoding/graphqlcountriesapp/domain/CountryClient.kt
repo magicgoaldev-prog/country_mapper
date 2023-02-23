933e33b131971cd41b63e174459dd598bfff9d61
magicgoal <magicgoal.dev@gmail.com>
2023-02-23 14:16:07 -0300

    getcountry suspend fun as well to return all the details of country

--- app/src/main/java/com/plcoding/graphqlcountriesapp/domain/CountryClient.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/domain/CountryClient.kt
@@ -3,4 +3,5 @@ package com.plcoding.graphqlcountriesapp.domain
 interface CountryClient {
 
     suspend fun getCountries(): List<SimpleCountry>
+    suspend fun getCountry(code: String) : DetailCountry?
 }
