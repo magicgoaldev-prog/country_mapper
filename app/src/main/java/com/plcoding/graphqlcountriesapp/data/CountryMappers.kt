70f50f48765fe878ca3524a560d191cf78b583a6
magicgoal <magicgoal.dev@gmail.com>
2023-02-23 14:33:38 -0300

    creating extension function to crountriesquery as well

--- app/src/main/java/com/plcoding/graphqlcountriesapp/data/CountryMappers.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/data/CountryMappers.kt
@@ -1,7 +1,9 @@
 package com.plcoding.graphqlcountriesapp.data
 
+import com.plcoding.CountriesQuery
 import com.plcoding.CountryQuery
 import com.plcoding.graphqlcountriesapp.domain.DetailCountry
+import com.plcoding.graphqlcountriesapp.domain.SimpleCountry
 
 
 fun CountryQuery.Country.toDetailCountry() : DetailCountry {
@@ -13,5 +15,14 @@ fun CountryQuery.Country.toDetailCountry() : DetailCountry {
         currency = code,
         languages = languages.mapNotNull {it.name},
         continent = continent.name
+    )
+}
+
+fun CountriesQuery.Country.toDetailCountry() : SimpleCountry {
+    return SimpleCountry(
+        code = code,
+        name = name,
+        emoji = emoji,
+        capital = capital?: "No capital"
     )
 }
