0f6729b96e6bba92bdfad450889b403bbf745986
magicgoal <magicgoal.dev@gmail.com>
2023-02-27 10:44:35 -0300

    logic to show screen

--- app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesScreen.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesScreen.kt
@@ -1,9 +1,13 @@
 package com.plcoding.graphqlcountriesapp.presentation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScopeInstance.align
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -14,7 +18,15 @@ fun CountriesScreen(
 ){
     Box(modifier = Modifier.fillMaxSize() ){
         if (state.isLoading){
-            CircularProgressIndicator()
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }else{
+            LazyColumn(modifier = Modifier.fillMaxSize()){
+                items(state.countries) { country ->
+
+                }
+            }
         }
     }
 }
