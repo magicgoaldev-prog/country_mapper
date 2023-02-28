63b68f97e76a9c8808f2d43b0f682b6bcca9feb8
magicgoal <magicgoal.dev@gmail.com>
2023-02-28 13:58:33 -0300

    validate modifier when click dialog

--- app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesScreen.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesScreen.kt
@@ -1,15 +1,19 @@
 package com.plcoding.graphqlcountriesapp.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -39,6 +43,16 @@ fun CountriesScreen(
                     )
                 }
             }
+        }
+        if (state.selectedCountry != null){
+            CountryDialog(
+                country = state.selectedCountry,
+                onDismiss =  onDismissCountryDialog,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Color.White)
+                    .padding(16.dp)
+            )
         }
     }
 }
@@ -77,7 +91,8 @@ private fun CountryDialog(
             Spacer(modifier = Modifier.width(8.dp))
             Text(text = "Capital: "+ country.capital)
             Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "Languague(s): "+ country.languages)
+            Text(text = "Languague(s): $joinedLanguages")
+            Spacer(modifier = Modifier.width(8.dp))
         }
     }
 }
